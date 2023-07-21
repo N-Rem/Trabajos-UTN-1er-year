d594cd96ec52fd7d10f2806c84064796d556701f
N-Rem <nicolasromero.barrios@gmail.com>
2023-07-21 15:56:53 -0300

    Arregla Bugs
    arregla porblemas a la hora de la imprecion de datos. Crea una constante para el maximo de ventas. Y una validacion para no entrar en una funcion entrar en un for inecesariamente.

--- Ejercicio-Integrador_U4.c
+++ Ejercicio-Integrador_U4.c
@@ -9,6 +9,7 @@
 #define IVA 1.21
 
 #define CANT_MAX 4
+#define VENT_MAX 9999
 #define CHAR_MAX 100
 struct Producto
 {
@@ -37,16 +38,17 @@ int validaCantidad(int v);
 
 void mostrarProductos(struct Producto p[]);
 void resumenDelDia(struct Venta v[], int numVent);
+int validaVenta(int numVenta);
 
 // array Global--
 float DESCUENTO[3] = {EFECTIVO, EFECTIVO, CREDITO};
 
 int main()
 {
-    int opcion = 0, vent = 0;
     struct Producto productos[CANT_MAX];
+    int opcion = 0, vent = 0;
     cargarProducto(productos);
-    struct Venta ventas[vent];
+    struct Venta ventas[VENT_MAX];
     do
     {
         printf("\n\t\tSelecione las siguientes opciones: \n 1. Registrar Ventas.\n 2. Ver listado de productos ordenados por descripcion.\n 3. Ver Resumen del dia.\n 4. Salir.\n");
@@ -63,14 +65,13 @@ int main()
             mostrarProductos(productos);
             break;
         case 3:
-            printf("\nResumen Del Dia: ");
             resumenDelDia(ventas, vent);
             break;
         case 4:
             printf("\n\tFin del progama.\n Gracias...");
             break;
         default:
-            printf("\n\t\tOpcion mal escrita..");
+            printf("\n\tOpcion mal escrita..");
             break;
         }
     } while (opcion != 4);
@@ -79,16 +80,16 @@ int main()
 
 void cargarProducto(struct Producto p[])
 {
-    p[0].codigo = 01;
+    p[0].codigo = 1;
     p[0].precio = 3500.00;
     strcpy(p[0].descripcion, "Mantel 2x2");
-    p[1].codigo = 02;
+    p[1].codigo = 2;
     p[1].precio = 800.99;
     strcpy(p[1].descripcion, "Plat playo 24cm");
-    p[2].codigo = 03;
+    p[2].codigo = 3;
     p[2].precio = 1450.50;
     strcpy(p[2].descripcion, "Copa vino");
-    p[3].codigo = 04;
+    p[3].codigo = 4;
     p[3].precio = 650.50;
     strcpy(p[3].descripcion, "Plato hondo 22cm");
 }
@@ -218,14 +219,30 @@ void mostrarProductos(struct Producto p[])
     printf("\nCodigo\t\tPrecio\t\t\tDescipcion.");
     for (int i = 0; i < CANT_MAX; i++)
     {
-        printf("\n%d\t\t%.2f\t\t\t%s\n", p[i].codigo, p[i].precio, p[i].descripcion);
+        printf("\n0%d\t\t%.2f\t\t\t%s\n", p[i].codigo, p[i].precio, p[i].descripcion);
+        printf("\n-------------------------------------------------\n");
     }
 }
 
 void resumenDelDia(struct Venta v[], int numVent)
 {
+    if (validaVenta(numVent)==0){
+        return;
+    }
+    printf("\nResumen Del Dia: ");
     for (int i = 0; i < numVent; i++)
     {
         printf("\nDNI: %ld \nPorsentaje de Descuento %.2f %% \nCodigo Producto %d \nCantidad Pedida: %d\nTotal: %.2f", v[i].dni, v[i].desc, v[i].codigo, v[i].cant, v[i].total);
+        printf("\n-------------------------------------------------\n");
+    
+    }
+}
+
+int validaVenta(int numVenta)
+{
+    if (numVenta == 0)
+    {
+        printf("\n\tNo hay ventas Registradas..");
+        return 0;
     }
 }
